a035e684ea15611240b9620ace5ff85285492ce6
ddemingjr22 <ddemingjr22@yahoo.com>
2017-08-29 20:46:13 -0400

    practice problems

--- FunWithLoops/Program.cs
+++ FunWithLoops/Program.cs
@@ -26,11 +26,11 @@ namespace FunWithLoops
             //        Console.WriteLine("that month is ok i guess....");
             //    }
             //}
-            string[] musicians = { "U2", "Maroon 5", "Ed Sheeran", "Nas", "Jarryd" };
-            foreach(string musician in musicians)
-            {
-                Console.WriteLine(musician);
-            }
+            //string[] musicians = { "U2", "Maroon 5", "Ed Sheeran", "Nas", "Jarryd" };
+            //foreach(string musician in musicians)
+            //{
+            //    Console.WriteLine(musician);
+            //}
 
             //Create a program that asks the user for their favorite food and stores those values in an array
             //Loop through the array and print all of the foods 
@@ -38,20 +38,130 @@ namespace FunWithLoops
             //create an array of lucky numbers using a foreach loop, print the following:
             //your lucky number is: 4
             //your lucky number is: 19
-            //your lucky number is 7
+            //your lucky number is: 7
 
-            Console.WriteLine("What are you three favorite foods?");
-            string foodOne = Console.ReadLine();
-            string foodTwo = Console.ReadLine();
-            string foodThree = Console.ReadLine();
-            string[] favoriteFoods = new string[3];
-            favoriteFoods[0] = foodOne;
-            favoriteFoods[1] = foodTwo;
-            favoriteFoods[2] = foodThree;
-            foreach (string foods in favoriteFoods)
-            {
-                Console.WriteLine(foods);
-            }
+            //Console.WriteLine("What are you three favorite foods?");
+            //string foodOne = Console.ReadLine();
+            //string foodTwo = Console.ReadLine();
+            //string foodThree = Console.ReadLine();
+            //string[] favoriteFoods = new string[3];
+            //favoriteFoods[0] = foodOne;
+            //favoriteFoods[1] = foodTwo;
+            //favoriteFoods[2] = foodThree;
+            //foreach (string foods in favoriteFoods)
+            //{
+            //    Console.WriteLine(foods);
+            //}
+
+            //int[] luckyNumbers = { 4, 19, 7 };
+            //foreach(int number in luckyNumbers)
+            //{
+            //    Console.WriteLine("Your lucky number is: " +number);
+            //}
+
+            //string[] Names = { "Jordan", "Max", "Peter" };
+            //for(int i = 0; i < Names.Length; i++)
+            //{
+            //    Names[i] = Names[i].ToLower();
+            //    Console.WriteLine(Names[i]);
+            //}
+
+            //for (int counter = 1; counter <= 10; counter++)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+            //for(int counterAgain =50; counterAgain <= 100; counterAgain++)
+            //{
+            //    Console.WriteLine(counterAgain);
+            //}
+
+            //string words = "I like cake";
+            //foreach(char letter in words)
+            //{
+            //    Console.WriteLine(letter);
+            //}
+
+            //string greetings = "My name is Dana";
+            //string[] words = greetings.Split();
+            //for(int i = 0; i < words.Length; i++)
+            //{
+            //    Console.WriteLine(words[i]);
+            //    if(words[i] == "Dana")
+            //    {
+            //        words[i] = "Max";
+            //    }
+            //}
+            //Console.WriteLine(words.[words.Length - 1]); 
+
+            //create an array called days with the elements monday - sunday
+            //use a forloop to print the elements to the console
+            //string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            //for(int i = 0; i < days.Length; i++)
+            //{
+            //    Console.WriteLine(days[i]);
+            //}
+
+            //create an int array size 25 put the numbers 0 - 25 in the array and print those numbers to the console *newkeyword
+            //int[] callNumber = new int[26];
+            //for (int i = 0; i <= 25; i++)
+            //{
+            //    callNumber[i] = i;
+            //    Console.WriteLine(callNumber[i]);
+            //}
+
+            //Start with the string "once upon a time" and create an array called storyWords using the split method.
+            //reverse the order of the elements in the array and using a for loop, print each word
+
+            //string words = "Once upon a time";
+            //string[] storyWords = words.Split();  //splitting on the white space
+            //Array.Reverse(storyWords);
+            //for (int i = 0; i < storyWords.Length; i++)
+            //{
+            //    Console.WriteLine(storyWords[i]);
+            //}
+
+            //while loops
+
+            //Console.WriteLine("Do you want to play a game");
+            //string playAgain = Console.ReadLine();
+            //while(playAgain == "Yes")
+            //{
+            //    Console.WriteLine("It's a rematch");
+            //    Console.WriteLine("Do you want to play again");
+            //    playAgain = Console.ReadLine();
+            //}
+
+            //Console.WriteLine("What is your favorite ROYGBIV color");
+            //string userColor = Console.ReadLine();
+            ////bool isROYGBIV = false;
+            //while(userColor != "red")
+            //{
+            //    Console.WriteLine("Sorry that is not the correct color, please try again");
+            //    userColor = Console.ReadLine();
+            //}
+
+            //do while loops
+            //string playAgain;
+            //do
+            //{
+            //    Console.WriteLine("Do you want to build a snowman?");
+            //    Console.WriteLine("That was fun");
+            //    Console.WriteLine("Do you want to build another snowman");
+            //    playAgain = Console.ReadLine();
+            //}
+            //while (playAgain == "yes");
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    if (i == 5)
+            //    {
+            //        break;
+            //    }
+            //}
+
+
 
         }
     }
